9d4f06ecfece652b1b4bb3108dea3bb1d3f7e5ef
Arthur Silveira <arthur.silveirasc@gmail.com>
2021-05-16 14:56:11 -0300

    Update sentimentalAnalysisFesteja.R

--- wordcloudTVFestival/sentimentalAnalysisFesteja.R
+++ wordcloudTVFestival/sentimentalAnalysisFesteja.R
@@ -1,6 +1,6 @@
 #Instala Pacotes
-#Depois de instalados, deix·-los como coment·rios para evitar dupla instalaÁ„o
-#ao executar o cÛdigo
+#Depois de instalados, deix√°-los como coment√°rios para evitar dupla instala√ß√£o
+#ao executar o c√≥digo
 install.packages("twitteR")
 install.packages("httpuv")
 install.packages("plyr")
@@ -33,25 +33,25 @@ api_key<- "Drx12FOUJMAS7DgzdCJtcRFYy"
 api_secret <- "XT9AyGRpxpfDNZXTjuFH8KUKyGjCPQWJjM7PQ9Bfo8oU7kfRXu"
 setup_twitter_oauth(api_key,api_secret)
 
-#Buscar 10.000 tweets sobre Festeja e analizar. ####
+#Buscar 10.000 tweets sobre Festeja e analisar. ####
 tweets = searchTwitter('festeja',n=10000, lang = "pt")
 Tweets.text = laply(tweets,function(t)t$getText()) #pega somente os textos dos Tweets
 
-#Limpa os tweets retirando pontuaÁ„o, colocando tudo em caixa baixa, ####
-#retirando espaÁos desnecess·rios, n˙meros, links, @pessoa, #hashtags, emoticons, etc...
-setwd("C:\\Users\\Arthur Silveira\\Dropbox\\Meus Documentos\\PÛs\\MÛdulo I\\Workshop I\\Mini Curso R")
+#Limpa os tweets retirando pontua√ß√£o, colocando tudo em caixa baixa, ####
+#retirando espa√ßos desnecess√°rios, n√∫meros, links, @pessoa, #hashtags, emoticons, etc...
+setwd("C:\\Users\\Arthur Silveira\\Dropbox\\Meus Documentos\\P√≥s\\M√≥dulo I\\Workshop I\\Mini Curso R")
 source("cleanTweets.R")
 Tweets.text = cleanTweets(Tweets.text)
 
 #Exportar para Excel ####
 write.xlsx(analysis, "myResults.xlsx")
 
-#Instalar pacotes para inÌcio da an·lise com wordcloud ####
+#Instalar pacotes para in√≠cio da an√°lise com wordcloud ####
 Needed <- c("tm", "SnowballC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")
 install.packages(Needed, dependencies = TRUE)
 install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/", type = "source")
 
-#Criar pasta no C: com nome "Texts", deixando o arquivo em Excel como ˙nico
+#Criar pasta no C: com nome "Texts", deixando o arquivo em Excel como √∫nico
 #elemento da pasta ####
 #Salvar arquivo em Excel como .txt
 cname <- file.path("C:", "texts")
@@ -59,13 +59,13 @@ cname <- file.path("C:", "texts")
 #Chamar biblioteca de text mining ####
 library(tm)
 
-#Adquirir lista de arquivos da pasta contendo o txt base para construÁ„o
+#Adquirir lista de arquivos da pasta contendo o txt base para constru√ß√£o
 #do WordCloud ####
 docs <- Corpus(DirSource(cname))
 
-#Remover pontuaÁıes, n˙meros e espaÁos em branco remanescentes. 
+#Remover pontua√ß√µes, n√∫meros e espa√ßos em branco remanescentes. 
 #Colocar todas as palavras em caixa baixa. Remover palavras que, 
-#atravÈs de an·lise empÌrica, n„o iriam agregar valor ‡ nuvem ####
+#atrav√©s de an√°lise emp√≠rica, n√£o iriam agregar valor √† nuvem ####
 docs <- tm_map(docs, removePunctuation)
 docs <- tm_map(docs, removeNumbers)
 docs <- tm_map(docs, tolower)
@@ -75,28 +75,28 @@ docs <- tm_map(docs, stripWhitespace)
 #Transformar documento em matriz ####
 dtm <- DocumentTermMatrix(docs)
 
-#An·lises de frequencia de palavras sobre o documento
+#An√°lises de frequencia de palavras sobre o documento
 freq <- colSums(as.matrix(dtm))
 length(freq)
 
 #Ordenar palavras das menos para as mais frequentes ####
 ord <- order(freq)
 
-#Consultar primeiras e ˙ltimas posiÁıes da matriz ####
+#Consultar primeiras e √∫ltimas posi√ß√µes da matriz ####
 freq[head(ord)]
 freq[tail(ord)]
 
-#Analisar n˙mero de palavras vs. quantidade de ocorrÍncias para as 20 primeiras
-# e ˙ltimas posiÁıes da matriz #### 
+#Analisar n√∫mero de palavras vs. quantidade de ocorr√™ncias para as 20 primeiras
+# e √∫ltimas posi√ß√µes da matriz #### 
 head(table(freq), 20)
 tail(table(freq), 20)
 
-#Ordenar ocorrÍncias de palavras na matriz da mais frequente para a menos
-# frequente e analisar primeiras posiÁıes
+#Ordenar ocorr√™ncias de palavras na matriz da mais frequente para a menos
+# frequente e analisar primeiras posi√ß√µes
 freq <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
 head(freq, 14)
 
-#Listar termos mais frequentes (a partir de, no mÌnimo, 15 ocorrÍncias)
+#Listar termos mais frequentes (a partir de, no m√≠nimo, 15 ocorr√™ncias)
 findFreqTerms(dtm, lowfreq = 15)
 
 #Plotar wordcloud
